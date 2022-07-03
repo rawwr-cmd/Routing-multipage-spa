ed0659a96eceb0565fae9f9c29c0fc04dd9e9a9e
rawwr <akshayace0302@gmail.com>
2022-07-04 01:57:39 +0530

    nested routes and redirecting the user

--- src/pages/WelcomePage.js
+++ src/pages/WelcomePage.js
@@ -1,5 +1,15 @@
+import { Route } from "react-router-dom";
+
 const Welcome = () => {
-  return <h1>The Welcome Page</h1>;
+  return (
+    <section>
+      <h1>The Welcome Page</h1>
+
+      <Route path="/welcome/new-user">
+        <p>WELCOME, NEW USER</p>
+      </Route>
+    </section>
+  );
 };
 
 export default Welcome;
